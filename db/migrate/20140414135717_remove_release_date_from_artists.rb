bfc6e76aa7449f875fa1ea0393a48208c410ed07
edEssential <ed@essential-music.com>
2014-04-14 16:33:47 +0100

    latest news styling done

--- db/migrate/20140414135717_remove_release_date_from_artists.rb
+++ db/migrate/20140414135717_remove_release_date_from_artists.rb
@@ -1,6 +1,6 @@
 class RemoveReleaseDateFromArtists < ActiveRecord::Migration
   def up
-    remove_column :artists, :release_date, :string
+    remove_column :artists, :release_date
   end
 
   def down
