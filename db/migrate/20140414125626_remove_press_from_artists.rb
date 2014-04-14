b973f1721d66aabe1c311a77cd9c7e1ee8680faf
edEssential <ed@essential-music.com>
2014-04-14 16:20:08 +0100

    latest news styling done

--- db/migrate/20140414125626_remove_press_from_artists.rb
+++ db/migrate/20140414125626_remove_press_from_artists.rb
@@ -1,7 +1,7 @@
 class RemovePressFromArtists < ActiveRecord::Migration
   def change
-    remove_column :artists, :press1, :string
-    remove_column :artists, :press2, :string
-    remove_column :artists, :press3, :string
+    remove_column :artists, :press1
+    remove_column :artists, :press2
+    remove_column :artists, :press3
   end
 end
