f732697ee443d005d2521acd524275d512b72109
edEssential <ed@essential-music.com>
2014-04-15 11:16:38 +0100

    bug fixes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,15 @@
 class ContactMailer < ActionMailer::Base
   
-  default from: "\"info@luaurecords.com\"<ed_hamilton@live.com>"
+  default from: "\"luaurecords@gmail.com\"<luaurecords@gmail.com>"
   
   def initiate_user(contact)
     @contact = contact
-    mail(to: @contact.email, subject: "Thankyou for getting in touch!")
+    mail(to: @contact.email, subject: "Thank you for getting in touch!")
   end
   
   def initiate_admin(contact)
     @contact = contact
-    mail(from: @contact.email, to: "ed_hamilton@live.com", bcc: "ed_hamilton@live.com", subject: "New message from the website!")
+    mail(from: @contact.email, to: "luaurecords@gmail.com", bcc: "ed_hamilton@live.com", subject: "New message from the website!")
   end
   
   def contacts_xls(contacts, user)
